6cd7309bb07955906fe18d1ef33c38537c97acaf
wamaekeige <mkeige@gmail.com>
2020-04-20 11:02:26 +0300

    display

---
+++ app/src/main/java/com/awake/display/ContactAdapter.java
@@ -0,0 +1,67 @@
+package com.awake.display;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ContactAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Contact contact = mContacts.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = viewHolder.messageButton;
+        button.setText(contact.isOnline() ? "Message" : "Offline");
+        button.setEnabled(contact.isOnline());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    // Store a member variable for the contacts
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameTextView;
+        public Button messageButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+
+    private List<Contact> mContacts;
+
+    public ContactAdapter(List<Contact> contacts) {
+        mContacts = contacts;
+    }
+}
---
+++ app/src/main/java/com/awake/display/MainActivity.java
@@ -0,0 +1,29 @@
+package com.awake.display;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Contact> contacts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactAdapter adapter = new ContactAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
